d6406696720f3b920ba2a90f4e8c33291e7164b5
Jesus Ramon Ramirez Medina <91562346+JesusR25@users.noreply.github.com>
2023-03-17 11:38:59 -0700

    Con gestos en terrestres

--- js/marcadores/Terrestre.js
+++ js/marcadores/Terrestre.js
@@ -129,3 +129,348 @@ function Perezoso(){
     allo = false;
     pere = true;
 }
+
+//Controlador para mammut
+AFRAME.registerComponent("mam", {
+    init: function () {
+        // track markerFound/markerLost
+        // grab the model reference
+        document.querySelector("#mammoth").addEventListener("model-loaded", evt => {
+            this.mesh = evt.detail.model
+        })
+        // hammerjs input helper
+        const hammertime = new Hammer(document.body);
+
+        // scale
+        // scale is tricky, because it resets
+        var currentScale = 1;
+        hammertime.get('pinch').set({ enable: true });
+        hammertime.on("pinchstart", (ev) => {
+            currentScale = this.mesh.scale.x;
+        })
+        hammertime.on("pinchmove", (ev) => {
+            if (!mamu) return;
+            this.mesh.scale.multiplyScalar(0).addScalar(ev.scale * currentScale);
+        });
+
+        // rotation
+        // pan left/right for rotation
+        this.isPanning = false;
+        var xrot = false;
+        hammertime.on("panleft", () => {
+            if (!mamu) return;
+            this.isPanning = true
+            this.mesh.rotation.y -= 4 * Math.PI / 360;
+        })
+
+        hammertime.on("panright", () => {
+            if (!mamu) return;
+            this.isPanning = true
+            this.mesh.rotation.y += 4 * Math.PI / 360;
+        })
+
+        hammertime.on("panup", () => {
+            if (!mamu) return;
+            xrot = true;
+            this.mesh.rotation.x -= 4 * Math.PI / 360;
+        })
+
+        hammertime.on("pandown", () => {
+            if (!mamu) return;
+            xrot = true;
+            this.mesh.rotation.x += 4 * Math.PI / 360;
+        })
+
+
+        hammertime.on("panend", () => this.isPanning = false, xrot = false)
+        hammertime.on("pancancel", () => this.isPanning = false, xrot = false)
+
+        hammertime.on("swipeleft", ({ velocity }) => {
+            if (!mamu) return;
+            this.swipeVelocity = velocity
+        })
+        hammertime.on("swiperight", ({ velocity }) => {
+            if (!mamu) return;
+            this.swipeVelocity = velocity
+        })
+        hammertime.on("swipeup", ({ velocity }) => {
+            if (!mamu) return;
+            this.swipeVelocity = velocity
+        })
+        hammertime.on("swipedown", ({ velocity }) => {
+            if (!mamu) return;
+            this.swipeVelocity = velocity
+        })
+    },
+    tick: function () {
+        if (!(mamu && this.swipeVelocity && !this.isPanning)){
+            return;
+        }else{
+            this.mesh.rotation.y += this.swipeVelocity * 4 * Math.PI / 360;
+            //this.mesh.rotation.x += this.swipeVelocity * 4 * Math.PI / 360;
+            this.swipeVelocity *= 0.93;
+            if (Math.abs(this.swipeVelocity) <= 0.1) this.swipeVelocity = 0;
+        }
+        
+        
+    }
+})
+
+//Controlador para Pterodactylus
+AFRAME.registerComponent("pet", {
+    init: function () {
+        // track markerFound/markerLost
+        // grab the model reference
+        document.querySelector("#mammoth").addEventListener("model-loaded", evt => {
+            this.mesh = evt.detail.model
+        })
+        // hammerjs input helper
+        const hammertime = new Hammer(document.body);
+
+        // scale
+        // scale is tricky, because it resets
+        var currentScale = 1;
+        hammertime.get('pinch').set({ enable: true });
+        hammertime.on("pinchstart", (ev) => {
+            currentScale = this.mesh.scale.x;
+        })
+        hammertime.on("pinchmove", (ev) => {
+            if (!pte) return;
+            this.mesh.scale.multiplyScalar(0).addScalar(ev.scale * currentScale);
+        });
+
+        // rotation
+        // pan left/right for rotation
+        this.isPanning = false;
+        var xrot = false;
+        hammertime.on("panleft", () => {
+            if (!pte) return;
+            this.isPanning = true
+            this.mesh.rotation.y -= 4 * Math.PI / 360;
+        })
+
+        hammertime.on("panright", () => {
+            if (!pte) return;
+            this.isPanning = true
+            this.mesh.rotation.y += 4 * Math.PI / 360;
+        })
+
+        hammertime.on("panup", () => {
+            if (!pte) return;
+            xrot = true;
+            this.mesh.rotation.x -= 4 * Math.PI / 360;
+        })
+
+        hammertime.on("pandown", () => {
+            if (!pte) return;
+            xrot = true;
+            this.mesh.rotation.x += 4 * Math.PI / 360;
+        })
+
+
+        hammertime.on("panend", () => this.isPanning = false, xrot = false)
+        hammertime.on("pancancel", () => this.isPanning = false, xrot = false)
+
+        hammertime.on("swipeleft", ({ velocity }) => {
+            if (!pte) return;
+            this.swipeVelocity = velocity
+        })
+        hammertime.on("swiperight", ({ velocity }) => {
+            if (!pte) return;
+            this.swipeVelocity = velocity
+        })
+        hammertime.on("swipeup", ({ velocity }) => {
+            if (!pte) return;
+            this.swipeVelocity = velocity
+        })
+        hammertime.on("swipedown", ({ velocity }) => {
+            if (!pte) return;
+            this.swipeVelocity = velocity
+        })
+    },
+    tick: function () {
+        if (!(pte && this.swipeVelocity && !this.isPanning)){
+            return;
+        }else{
+            this.mesh.rotation.y += this.swipeVelocity * 4 * Math.PI / 360;
+            //this.mesh.rotation.x += this.swipeVelocity * 4 * Math.PI / 360;
+            this.swipeVelocity *= 0.93;
+            if (Math.abs(this.swipeVelocity) <= 0.1) this.swipeVelocity = 0;
+        }
+        
+        
+    }
+})
+
+//Controlador para Allosaurus
+AFRAME.registerComponent("allo", {
+    init: function () {
+        // track markerFound/markerLost
+        // grab the model reference
+        document.querySelector("#mammoth").addEventListener("model-loaded", evt => {
+            this.mesh = evt.detail.model
+        })
+        // hammerjs input helper
+        const hammertime = new Hammer(document.body);
+
+        // scale
+        // scale is tricky, because it resets
+        var currentScale = 1;
+        hammertime.get('pinch').set({ enable: true });
+        hammertime.on("pinchstart", (ev) => {
+            currentScale = this.mesh.scale.x;
+        })
+        hammertime.on("pinchmove", (ev) => {
+            if (!allo) return;
+            this.mesh.scale.multiplyScalar(0).addScalar(ev.scale * currentScale);
+        });
+
+        // rotation
+        // pan left/right for rotation
+        this.isPanning = false;
+        var xrot = false;
+        hammertime.on("panleft", () => {
+            if (!allo) return;
+            this.isPanning = true
+            this.mesh.rotation.y -= 4 * Math.PI / 360;
+        })
+
+        hammertime.on("panright", () => {
+            if (!allo) return;
+            this.isPanning = true
+            this.mesh.rotation.y += 4 * Math.PI / 360;
+        })
+
+        hammertime.on("panup", () => {
+            if (!allo) return;
+            xrot = true;
+            this.mesh.rotation.x -= 4 * Math.PI / 360;
+        })
+
+        hammertime.on("pandown", () => {
+            if (!allo) return;
+            xrot = true;
+            this.mesh.rotation.x += 4 * Math.PI / 360;
+        })
+
+
+        hammertime.on("panend", () => this.isPanning = false, xrot = false)
+        hammertime.on("pancancel", () => this.isPanning = false, xrot = false)
+
+        hammertime.on("swipeleft", ({ velocity }) => {
+            if (!allo) return;
+            this.swipeVelocity = velocity
+        })
+        hammertime.on("swiperight", ({ velocity }) => {
+            if (!allo) return;
+            this.swipeVelocity = velocity
+        })
+        hammertime.on("swipeup", ({ velocity }) => {
+            if (!allo) return;
+            this.swipeVelocity = velocity
+        })
+        hammertime.on("swipedown", ({ velocity }) => {
+            if (!allo) return;
+            this.swipeVelocity = velocity
+        })
+    },
+    tick: function () {
+        if (!(allo && this.swipeVelocity && !this.isPanning)){
+            return;
+        }else{
+            this.mesh.rotation.y += this.swipeVelocity * 4 * Math.PI / 360;
+            //this.mesh.rotation.x += this.swipeVelocity * 4 * Math.PI / 360;
+            this.swipeVelocity *= 0.93;
+            if (Math.abs(this.swipeVelocity) <= 0.1) this.swipeVelocity = 0;
+        }
+        
+        
+    }
+})
+
+
+//Controlador para Perezoso
+AFRAME.registerComponent("pere", {
+    init: function () {
+        // track markerFound/markerLost
+        // grab the model reference
+        document.querySelector("#mammoth").addEventListener("model-loaded", evt => {
+            this.mesh = evt.detail.model
+        })
+        // hammerjs input helper
+        const hammertime = new Hammer(document.body);
+
+        // scale
+        // scale is tricky, because it resets
+        var currentScale = 1;
+        hammertime.get('pinch').set({ enable: true });
+        hammertime.on("pinchstart", (ev) => {
+            currentScale = this.mesh.scale.x;
+        })
+        hammertime.on("pinchmove", (ev) => {
+            if (!pere) return;
+            this.mesh.scale.multiplyScalar(0).addScalar(ev.scale * currentScale);
+        });
+
+        // rotation
+        // pan left/right for rotation
+        this.isPanning = false;
+        var xrot = false;
+        hammertime.on("panleft", () => {
+            if (!pere) return;
+            this.isPanning = true
+            this.mesh.rotation.y -= 4 * Math.PI / 360;
+        })
+
+        hammertime.on("panright", () => {
+            if (!pere) return;
+            this.isPanning = true
+            this.mesh.rotation.y += 4 * Math.PI / 360;
+        })
+
+        hammertime.on("panup", () => {
+            if (!pere) return;
+            xrot = true;
+            this.mesh.rotation.x -= 4 * Math.PI / 360;
+        })
+
+        hammertime.on("pandown", () => {
+            if (!pere) return;
+            xrot = true;
+            this.mesh.rotation.x += 4 * Math.PI / 360;
+        })
+
+
+        hammertime.on("panend", () => this.isPanning = false, xrot = false)
+        hammertime.on("pancancel", () => this.isPanning = false, xrot = false)
+
+        hammertime.on("swipeleft", ({ velocity }) => {
+            if (!pere) return;
+            this.swipeVelocity = velocity
+        })
+        hammertime.on("swiperight", ({ velocity }) => {
+            if (!pere) return;
+            this.swipeVelocity = velocity
+        })
+        hammertime.on("swipeup", ({ velocity }) => {
+            if (!pere) return;
+            this.swipeVelocity = velocity
+        })
+        hammertime.on("swipedown", ({ velocity }) => {
+            if (!pere) return;
+            this.swipeVelocity = velocity
+        })
+    },
+    tick: function () {
+        if (!(pere && this.swipeVelocity && !this.isPanning)){
+            return;
+        }else{
+            this.mesh.rotation.y += this.swipeVelocity * 4 * Math.PI / 360;
+            //this.mesh.rotation.x += this.swipeVelocity * 4 * Math.PI / 360;
+            this.swipeVelocity *= 0.93;
+            if (Math.abs(this.swipeVelocity) <= 0.1) this.swipeVelocity = 0;
+        }
+        
+        
+    }
+})
