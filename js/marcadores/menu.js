506d20d3e09aff0386230a9ba44eb9b17f02de7f
Jesus Ramon Ramirez Medina <91562346+JesusR25@users.noreply.github.com>
2023-03-23 09:42:50 -0700

    Alertar actualizadas

--- js/marcadores/menu.js
+++ js/marcadores/menu.js
@@ -9,8 +9,8 @@ function advertencia(){
   
   function Humanos(){
     Swal.fire({
-      title: 'Historia del Meteorito de Bacubirito',
-      html: '•	En esta escena está disponible el marcador <b> Video. </b> <br><br> •	Conoce los principales acontecimientos del Meteorito, escanea el marcador o presiona el botón <b>Video</b> para reproducir el video. <br><br> •	Se sugiere coloque su dispositivo móvil en <b>posición vertical</b> con el fin de tener una mejor visualización.',
+      title: 'Evolución del Ser Humano',
+      html: '•	Conoce los principales acontecimientos del ser humano a través de la historia.<br><br> •	Para ello están disponibles los modelos Paranthropus, Australopithecus, Homo Sapiens, Homo Hábilis, Homo Erectus y Neanderthal, que pueden ser visualizados a través de posicionar el marcador sobre la cámara o a través de seleccionar el botón correspondiente del menú Elementos 3D.<br><br> •	En pantalla aparecerá su representación en 3D con su respectiva ficha técnica.',
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
@@ -26,8 +26,8 @@ function advertencia(){
   
   function Terrestres(){
     Swal.fire({
-      title: 'Meteorito de Bacubirito',
-      html: '•	En esta pantalla visualizaras una representación en 3D del Meteorito de Bacubirito, podrás rotarlo, acercarlo o alejarlo con los gestos que utilices en la pantalla. <br><br> •	Se sugiere coloque su dispositivo móvil en <b>posición vertical</b> con el fin de tener una mejor visualización.' ,
+      title: 'Restos fósiles de animales terrestres',
+      html: '•	Conoce la información de algunas criaturas que habitan o habitaron sobre la superficie terrestre. Para ello están disponibles los modelos “Mammut", "Perezoso", "Pterodactylus" y "Allosaurus". <br><br> •	Escanea el marcador correspondiente o selecciona el botón correspondiente del menú Elementos 3D.<br><br> •	En pantalla aparecerá su representación en 3D con su respectiva ficha técnica.' ,
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
@@ -43,8 +43,8 @@ function advertencia(){
   
   function Acuaticos(){
     Swal.fire({
-      title: 'Composición Química del Meteorito',
-      html: '•	En esta escena están disponibles los marcadores <b> Composición </b> y <b> Video</b>. <br><br>	•	Escanea el marcador <b>Video</b> o presiona el botón <b>Video</b> para reproducir el video que relata la composición química del Meteorito de Bacubirito.<br><br>• Escanea el marcador de <b>Composición</b> o presiona el botón <b>Composición</b> para visualizar qué elementos químicos forman parte del meteorito.',
+      title: 'Fósiles de animales acuáticos',
+      html: '•	Conoce la información de algunas criaturas que habitan o habitaron en los océanos.<br><br>	•	Para ello están disponibles los modelos "Mosasaurus", "Megalodón", "Ballena", "Tiburón" y "Celacanto". <br><br>• Escanea el marcador correspondiente o selecciona el botón correspondiente del menú Elementos 3D.<br><br> •	En pantalla aparecerá su representación en 3D con su respectiva ficha técnica. ',
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
@@ -60,8 +60,8 @@ function advertencia(){
 
   function Fosiles(){
     Swal.fire({
-      title: 'Composición Química del Meteorito',
-      html: '•	En esta escena están disponibles los marcadores <b> Composición </b> y <b> Video</b>. <br><br>	•	Escanea el marcador <b>Video</b> o presiona el botón <b>Video</b> para reproducir el video que relata la composición química del Meteorito de Bacubirito.<br><br>• Escanea el marcador de <b>Composición</b> o presiona el botón <b>Composición</b> para visualizar qué elementos químicos forman parte del meteorito.',
+      title: 'Otros Restos Fósiles',
+      html: '•	En esta escena se muestran algunos otros restos fósiles, algunos habitaron sobre la superficie terrestre y otros en los océanos.<br><br>	•	Se encuentran disponibles los modelos "Belemnite", "Trilobite", "Ammonite" y "Fósil Vegetal". <br><br>• Escanea el marcador o selecciona el botón correspondiente del menú Elementos 3D. <br><br> •	En pantalla aparecerá su representación en 3D con su respectiva ficha técnica.',
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
