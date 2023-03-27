106ccd572fab72de783d6aa354814f79a6eb8f35
Jesus Ramon Ramirez Medina <91562346+JesusR25@users.noreply.github.com>
2023-03-27 12:25:31 -0700

    Rotacion en todas

--- js/marcadores/Acuaticos.js
+++ js/marcadores/Acuaticos.js
@@ -601,3 +601,17 @@ function volver(){
 function ocultar(){
     document.getElementById('imagenes').style.display = "none";
 }
+
+function readDeviceOrientation() {
+    if (Math.abs(window.orientation) === 90) {
+        //Landscape
+        document.getElementById("inf").style.marginLeft = "3%";
+        document.getElementById("inf").style.width = "23%";
+        document.getElementById("inf").style.marginTop = "3%";
+    } else {
+        document.getElementById("inf").style.marginLeft = "20%";
+        document.getElementById("inf").style.width = "56%";
+        document.getElementById("inf").style.marginTop = "15%";
+    }
+}
+screen.orientation.addEventListener("change", readDeviceOrientation);
