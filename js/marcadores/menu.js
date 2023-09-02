502dc87a453a4c783cf97c2e133272d0c8a31c69
Jesus Ramon Ramirez Medina <91562346+JesusR25@users.noreply.github.com>
2023-09-02 06:42:59 -0700

    Update menu.js

--- js/marcadores/menu.js
+++ js/marcadores/menu.js
@@ -159,3 +159,66 @@ function advertencia(){
       }
     })
   }
+
+function Humanos2(){
+    Swal.fire({
+      title: 'Evolución del Ser Humano',
+      html: '•	Conoce los principales acontecimientos del ser humano a través de la historia.<br><br> •	Para ello están disponibles los modelos Paranthropus, Australopithecus, Homo Sapiens, Homo Hábilis, Homo Erectus y Neanderthal, que pueden ser visualizados a través de posicionar el marcador sobre la cámara o a través de seleccionar el botón correspondiente del menú Elementos 3D.<br><br> •	En pantalla aparecerá su representación en 3D con su respectiva ficha técnica.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Aceptar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+      }
+    })
+  }
+  
+  function Terrestres2(){
+    Swal.fire({
+      title: 'Restos fósiles de animales terrestres',
+      html: '•	Conoce la información de algunas criaturas que habitan o habitaron sobre la superficie terrestre. Para ello están disponibles los modelos “Mammut", "Perezoso", "Pterodactylus" y "Allosaurus". <br><br> •	Escanea el marcador correspondiente o selecciona el botón correspondiente del menú Elementos 3D.<br><br> •	En pantalla aparecerá su representación en 3D con su respectiva ficha técnica.' ,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Aceptar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+      }
+    })
+  }
+  
+  function Acuaticos2(){
+    Swal.fire({
+      title: 'Fósiles de animales acuáticos',
+      html: '•	Conoce la información de algunas criaturas que habitan o habitaron en los océanos.<br><br>	•	Para ello están disponibles los modelos "Mosasaurus", "Megalodón", "Ballena", "Tiburón" y "Celacanto". <br><br>• Escanea el marcador correspondiente o selecciona el botón correspondiente del menú Elementos 3D.<br><br> •	En pantalla aparecerá su representación en 3D con su respectiva ficha técnica. ',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Aceptar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+      }
+    })
+  }
+
+  function Fosiles2(){
+    Swal.fire({
+      title: 'Otros Restos Fósiles',
+      html: '•	En esta escena se muestran algunos otros restos fósiles, algunos habitaron sobre la superficie terrestre y otros en los océanos.<br><br>	•	Se encuentran disponibles los modelos "Belemnite", "Trilobite", "Ammonite" y "Fósil Vegetal". <br><br>• Escanea el marcador o selecciona el botón correspondiente del menú Elementos 3D. <br><br> •	En pantalla aparecerá su representación en 3D con su respectiva ficha técnica.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Aceptar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+      }
+    })
